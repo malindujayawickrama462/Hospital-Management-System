d5c3a09a913fa1a86a27d66c9927b05f6d4fe843
malindujayawickrama462 <168506653+malindujayawickrama462@users.noreply.github.com>
2025-08-02 20:40:29 +0530

    body parser and nodemon added

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,14 @@
+//importing the Express framework into your project.
 import express from 'express';
+import bodyParser from 'body-parser';
 
+//this creates web server
 let app = express();
 
+app.use(bodyParser.json());
+
+// Starting the server and making it listen on port 3001
+// Once the server is running, it will log a message to the console
 app.listen(3001,()=>{
     console.log('Server running on port 3001')
 });
