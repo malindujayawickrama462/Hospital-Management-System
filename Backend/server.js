4f7a1bfc34f989001afbd6fc48060d03c317caff
malindujayawickrama462 <168506653+malindujayawickrama462@users.noreply.github.com>
2025-08-07 10:51:33 +0530

    mondodb connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,14 @@
 //importing the Express framework into your project.
 // Importing body-parser middleware to handle JSON request bodies
+//import ODM library to interact with mongodb
+//import dtenv to store sensitive data
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+
+//load environment variables
+dotenv.config()
 
 //this creates web server
 let app = express();
@@ -10,8 +17,15 @@ let app = express();
 // This allows your server to read and understand JSON data sent in the request body
 app.use(bodyParser.json());
 
+//connect to mongodb
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("connected to the database")
+}).catch(()=>{
+    console.log("connection failed")
+})
+
 // Starting the server and making it listen on port 3001
 // Once the server is running, it will log a message to the console
-app.listen(3001,()=>{
+app.listen(process.env.PORT,()=>{
     console.log('Server running on port 3001')
 });
