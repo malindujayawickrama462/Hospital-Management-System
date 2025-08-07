298f6e4646a9e26db878d9b5974b4b1dfab257b5
malindujayawickrama462 <168506653+malindujayawickrama462@users.noreply.github.com>
2025-08-07 12:11:33 +0530

    hashed the password

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -33,9 +33,30 @@ const userSchema = mongoose.Schema(
             type : String,
             required : true,
             default : "https://eu.ui-avatars.com/api/?name=John+Doe&size=250"
-        }
+        },
+        
+    },
+    {
+        timestamps: true,
     }
 );
+
+//pre-save middelware to hash password
+
+userSchema.pre("save", async function(next){
+    //only hash the password if it has been modified(or is new)
+    if(!this.isModified("password")){
+        return next();
+    }
+    try{
+        const salt = await bcrypt.gensalt(10);//genarate salt
+        const hashedPassword = await bcrypt.hash(this.password, salt);//hash the password
+        this.password = hashedPassword;//replace plain password
+        next();
+    }catch(err){
+        next(err);//pass error
+    }
+});
 
 const User = mongoose.model("Users",userSchema);
 
